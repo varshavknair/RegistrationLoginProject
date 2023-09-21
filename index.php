0991adcc762f098c4f6e3107377b09d7039540cf
Varsha Nair <varshavknair@gmail.com>
2023-09-21 22:28:45 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Registration Form</title>
+	<link rel="stylesheet" href="styles.css">
+	</head>
+	<body>
+		<div class="container">
+			<div class="registration-box">
+				<h2>Registration Form</h2>
+				<form action="#" method="POST">
+					<div class="input-container">
+						<input type="text" name="name" required>
+						<label for="name">Full Name</label>
+					</div>
+					<div class="input-container">
+						<input type="email" name="email" required>
+						<label for="email">Email</label>
+					</div>
+					<div class="input-container">
+						<input type="password" name="password" required>
+						<label for="password">Password</label>
+					</div>
+					<div class="input-container">
+						<input type="password" name="confirm-password" required>
+						<label for="confirm-password">Confirm Password</label>
+					</div>
+					<button type="submit">Register</button>
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
