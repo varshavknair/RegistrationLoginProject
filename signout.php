f9aa4265f38f99abbaf15ddb2fc7936c5232bc94
Varsha Nair <varshavknair@gmail.com>
2023-09-26 18:23:04 +0530

    Update signout.php

--- signout.php
+++ signout.php
@@ -1,7 +1,7 @@
 <?php
 	require_once((dirname(__FILE__)) . '/helpers/common.php');
 	session_unset();
-    session_destroy();
+	session_destroy();
 
 	/* This will give an error. Note the output
 	* above, which is before the header() call */
