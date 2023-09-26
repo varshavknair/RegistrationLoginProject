a634e049b80605136f3eff6f3f86f21ad58665e8
Varsha Nair <varshavknair@gmail.com>
2023-09-26 17:24:14 +0530

    Update header.php

--- header.php
+++ header.php
@@ -6,10 +6,10 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="bootstrap.min.css" rel="stylesheet">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
 
-		<link rel="stylesheet" href="main.css" />
-		<link rel="stylesheet" href="color.css" />
+		<link rel="stylesheet" href="css/main.css" />
+		<link rel="stylesheet" href="css/color.css" />
 
 		<script src="js/jquery.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
